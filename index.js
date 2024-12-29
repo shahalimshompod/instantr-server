aa4b72fa93c3fbf04336fd245fc730158043eb50
shahalimshompod <shahalimsompod@gmail.com>
2024-12-30 00:33:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ezm1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // database collection
+    const instantRBlogs = client.db('InstantRDatabase').collection('instant-r-all-blogs')
+
+    // GET OPERATIONS
+    // get operation for blogs
+    app.get('/section/blogs', async (req, res)=> {
+      const cursor = instantRBlogs.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // get operation for home with limit
+    app.get('/featured-blogs', async (req, res) => {
+      const cursor = instantRBlogs.find().limit(15);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+  res.send('INSTANT R IS RUNNING')
+})
+
+app.listen(port, () => {
+  console.log(`INSTANT R IS RUNNING ON PORT ${port}`);
+})
