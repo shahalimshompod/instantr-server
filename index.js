05264559075d12a2634201f97a113ea74e8d5dec
shahalimshompod <shahalimsompod@gmail.com>
2025-03-20 20:23:23 +0600

    done functionalities

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfxvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
